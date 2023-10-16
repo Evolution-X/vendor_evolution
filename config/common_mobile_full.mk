148bcd6550e62d69958e1ee47b8771123bc1e1d4
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:58:04 +0530

    evolution: Remove packages moved to extras
    Change-Id: I6af87bbb53245a407ca2566c0d708ef0da5f8b4c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,10 +3,6 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
 # Apps
 PRODUCT_PACKAGES += \
     Etar \
@@ -28,12 +24,6 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontLatoOverlay \
-    FontRubikOverlay
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries

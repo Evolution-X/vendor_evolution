0a69cd381eae32cb5598f6f6668ab42915deae1f
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    evolution: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/evolution.mk
+++ config/evolution.mk
@@ -12,6 +12,10 @@ PRODUCT_PACKAGES += \
 endif
 endif
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml
+
 # Private keys
 ifeq ($(EVO_BUILD_TYPE),Official)
 include vendor/evolution-priv/keys/keys.mk

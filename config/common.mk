695089087470608e1d84f6346c6ad247c82069dd
Dyneteve <dyneteve@hentaios.com>
2024-04-24 19:09:04 +0900

    evolution: Inherit Pixel additions via `WITH_GMS`
    * GMS
    * Audio
    * Charger
    * RRO overlays
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,11 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/lineage/config/evolution.mk)
 $(call inherit-product, vendor/extras/evolution.mk)
 
+# Pixel additions
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/pixel-style/config/common.mk)
+endif
+
 PRODUCT_BRAND ?= EvolutionX
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -145,7 +150,6 @@ PRODUCT_COPY_FILES += \
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig \
     SimpleSettingsConfig
 
 # Extra tools in Lineage
@@ -196,9 +200,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     rsync
 
+ifeq ($(WITH_GMS),false)
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
+endif
 
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
@@ -236,9 +242,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 
 # SetupWizard
+ifeq ($(WITH_GMS),false)
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v4 \
     setupwizard.feature.day_night_mode_enabled=true
+endif
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \

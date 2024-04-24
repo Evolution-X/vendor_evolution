558a8a6e45d183e7ce75ad6b2ed7aae28b7d5ea2
Dyneteve <dyneteve@hentaios.com>
2024-04-24 19:09:04 +0900

    evolution: Inherit Pixel additions via `WITH_GMS`
    * GMS
    * Audio
    * Charger
    * RRO overlays
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -9,17 +9,23 @@ PRODUCT_PACKAGES += \
     Etar \
     Profiles \
     Recorder \
-    Seedvault \
+    Seedvault
+
+ifeq ($(WITH_GMS),false)
+PRODUCT_PACKAGES += \
     Twelve
+endif
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
     Aperture
 endif
 
+ifeq ($(WITH_GMS),false)
 ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
 PRODUCT_PACKAGES += \
     AudioFX
+endif
 endif
 
 # Extra cmdline tools

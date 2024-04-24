e2c763a0fbe2e71bfd6b027530d5c40e7dbb583d
Dyneteve <dyneteve@hentaios.com>
2024-04-24 19:09:04 +0900

    evolution: Inherit Pixel additions via `WITH_GMS`
    * GMS
    * Audio
    * Charger
    * RRO overlays
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -5,3 +5,9 @@ $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 $(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/wifionly.mk)
+
+# GMS
+WITH_GMS ?= true
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/gms/gms_full_tablet_wifionly.mk)
+endif

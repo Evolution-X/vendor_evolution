388cf9688d31fd623abb81c60a97343d96f97749
AnierinB <anierin@evolution-x.org>
2024-08-18 11:59:09 +0900

    evolution: Configure support for Evolution X Vanilla
    Change-Id: I39afafdd59cb641130701a22bc1d3ecaf57754e5
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/version.mk
+++ config/version.mk
@@ -2,8 +2,20 @@ PRODUCT_VERSION_MAJOR = 17
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION := 12.0
+EVO_VERSION_BASE := 12.0
 EVO_BUILD_TYPE ?= Unofficial
+
+ifeq ($(EVO_BUILD_TYPE),Official)
+else ifeq ($(EVO_BUILD_TYPE),Unofficial)
+else
+$(error EVO_BUILD_TYPE must be Official or Unofficial. Current value: $(EVO_BUILD_TYPE))
+endif
+
+ifeq ($(WITH_GMS),true)
+EVO_VERSION := $(EVO_VERSION_BASE)
+else
+EVO_VERSION := $(EVO_VERSION_BASE)-Vanilla
+endif
 
 # Internal version
 LINEAGE_VERSION := EvolutionX-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(EVO_VERSION)-$(EVO_BUILD_TYPE)
@@ -13,6 +25,7 @@ LINEAGE_DISPLAY_VERSION := v$(EVO_VERSION)-$(shell date +%Y%m%d)
 
 # Evolution X version properties
 PRODUCT_PRODUCT_PROPERTIES += \
+    ro.evolution.build.type=$(EVO_BUILD_TYPE) \
     ro.evolution.build.version=$(LINEAGE_VERSION) \
     ro.evolution.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.evolution.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \

d8e7a123c7aae8b17e3a1d37ada861777fb9bf4a
AnierinB <anierin@evolution-x.org>
2024-08-18 11:59:09 +0900

    evolution: Configure support for Evolution X Vanilla
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/version.mk
+++ config/version.mk
@@ -2,8 +2,14 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION := 11.0
+EVO_VERSION_BASE := 11.0
 EVO_BUILD_TYPE ?= Unofficial
+
+ifeq ($(WITH_GMS),true)
+EVO_VERSION := $(EVO_VERSION_BASE)
+else
+EVO_VERSION := $(EVO_VERSION_BASE)-Vanilla
+endif
 
 # Internal version
 LINEAGE_VERSION := EvolutionX-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(EVO_VERSION)-$(EVO_BUILD_TYPE)

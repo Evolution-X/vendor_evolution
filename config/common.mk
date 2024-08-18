f3b80aa49525ce6ed95edcbab32041008a053c9a
Le Hong Duc <hongducthbk123@gmail.com>
2024-08-18 08:54:52 +0700

    config: Don't dexpreopt prebuilt apps
    Pre-optimizing prebuilt Gapps is not needed because they are mostly
    updated via Play Store and device will optimize them on install.
    
    Save some space on /product partition and reduce build time.
    
    Signed-off-by: Le Hong Duc <hongducthbk123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -13,6 +13,9 @@ PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
 ifeq ($(WITH_GMS),true)
 $(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
 $(call inherit-product, vendor/pixel-style/config/common.mk)
+
+# Don't dexpreopt prebuilts. (For GMS).
+DONT_DEXPREOPT_PREBUILTS := true
 endif
 
 PRODUCT_BRAND ?= EvolutionX

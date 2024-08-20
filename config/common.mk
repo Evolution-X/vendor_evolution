754eb6dba9acae847e2c66d1ee328d44ae163e01
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-20 18:32:22 +0900

    vendor: Introduce bootanimation styles [2/3]
    vendor: Add more bootanimation styles [2/3]
    
    vendor: Add ctOS bootanimation [2/3]
    
    by benmeroff - https://xdaforums.com/t/boot-animation-ctos-boot-animation.2843830/
    
    vendor: Add Evolution X Reveal boot animation [2/3]
    
    vendor: Add more classic boot animation styles [2/3]
    
    * Remove anniv bootanimation as it takes a lot of space.
    
    Co-authored-by: 0xsharkboy <achille@0xsharkboy.dev>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: 0xsharkboy <achille@0xsharkboy.dev>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -127,6 +127,10 @@ $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUI
 endif
 
 # Bootanimation
+TARGET_SUPPORT_BOOT_ANIMATIONS ?= true
+ifeq ($(TARGET_SUPPORT_BOOT_ANIMATIONS),true)
+include vendor/lineage/bootanimation/prebuilts/prebuilts.mk
+endif
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \

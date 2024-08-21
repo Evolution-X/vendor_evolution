480c2a00eceefcc4e3076900eda8f9a9ca533b7d
Alvin Francis <nivlafx@gmail.com>
2024-08-20 21:17:45 -0400

    etc: Add bootanim init
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Bootanim
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.bootanim.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.bootanim.rc
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleSettingsConfig

adceaac44388882a0f4ce46749e3545378cb4275
tejas101k <tejassingh649@rediffmail.com>
2025-01-20 09:28:51 +0000

    config: Build Updater for all
    * Local update could still be helpful

--- config/evolution.mk
+++ config/evolution.mk
@@ -2,14 +2,12 @@
 PRODUCT_PACKAGES += \
     EvoEgg
 
-ifeq ($(EVO_BUILD_TYPE),Official)
 PRODUCT_PACKAGES += \
     Updater
 
 ifeq ($(WITH_GMS),false)
 PRODUCT_PACKAGES += \
     UpdaterVanillaOverlay
-endif
 endif
 
 # BtHelper

4c4f5b04ff50c4f4aa83615c078dffb5e22e0e20
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    config: properties: Add pihooks default values
    * Android 17 Beta 2
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,19 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=blazer_beta \
+    persist.sys.pihooks_DEVICE?=blazer \
+    persist.sys.pihooks_ID?=CP21.260306.017 \
+    persist.sys.pihooks_RELEASE?=17 \
+    persist.sys.pihooks_SECURITY_PATCH?=2026-03-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/blazer_beta/blazer:17/CP21.260306.017/15063635:user/release-keys" \
+    PihooksGmsModel="Pixel 10 Pro"
+
 # Build fingerprint override
 ENABLE_FINGERPRINT_OVERRIDE ?= false
 ifeq ($(filter Google google,$(PRODUCT_MANUFACTURER)),)

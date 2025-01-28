e7bff4108094dfd393fc7e3a3aef0ae268f22486
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    config: properties: Add pihooks default values
    * Android 17 Beta 2
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,19 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=blazer_beta \
+    persist.sys.pihooks_DEVICE?=blazer \
+    persist.sys.pihooks_ID?=CP21.260206.011 \
+    persist.sys.pihooks_RELEASE?=16 \
+    persist.sys.pihooks_SECURITY_PATCH?=2026-02-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/blazer_beta/blazer:CinnamonBun/CP21.260206.011/14911669:user/release-keys" \
+    PihooksGmsModel="Pixel 10 Pro"
+
 # Evolution X packages
 PRODUCT_PACKAGES += \
     EvoEgg \

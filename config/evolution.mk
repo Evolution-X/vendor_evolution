d693fd4d68b2f98cead56699e1d761ff5a2ff919
Joey Huab <jhuab@edukasyon.ph>
2025-02-22 21:53:08 +0900

    config: properties: March 2025 pihooks update

--- config/evolution.mk
+++ config/evolution.mk
@@ -2,15 +2,15 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=comet_beta \
-    persist.sys.pihooks_DEVICE?=comet \
-    persist.sys.pihooks_ID?=BP22.250103.008 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-01-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=35
+    persist.sys.pihooks_PRODUCT?=oriole_beta \
+    persist.sys.pihooks_DEVICE?=oriole \
+    persist.sys.pihooks_ID?=BP22.250221.013 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-03-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/comet_beta/comet:Baklava/BP22.250103.008/12932282:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro Fold"
+    PihooksGmsFp="google/oriole_beta/oriole:16/BP22.250221.013/13218340:user/release-keys" \
+    PihooksGmsModel="Pixel 6"
 
 # Evolution X packages
 PRODUCT_PACKAGES += \

f63d3ed3c552c63773251311cd47d6286ab87631
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-05 13:25:32 +0800

    config: Add bypass charging prop
    Change-Id: Iee2e99e9ec929221336fea361ab516b00f47bb7e

--- config/evolution.mk
+++ config/evolution.mk
@@ -75,3 +75,7 @@ include vendor/evolution-priv/keys/keys.mk
 else
 -include vendor/evolution-priv/keys/keys.mk
 endif
+
+BYPASS_CHARGE_SUPPORTED ?= false
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED)

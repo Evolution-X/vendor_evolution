70fd45de735d5fb2ddf524ac180899c2d1c15b88
Joey Huab <jhuab@edukasyon.ph>
2025-04-19 00:14:13 +0900

    config: Update pihooks to Pixel Android 16.0 Beta 4

--- config/evolution.mk
+++ config/evolution.mk
@@ -2,15 +2,15 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=oriole_beta \
-    persist.sys.pihooks_DEVICE?=oriole \
-    persist.sys.pihooks_ID?=BP22.250221.013 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-03-05 \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP22.250325.007 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-04-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/oriole_beta/oriole:16/BP22.250221.013/13218340:user/release-keys" \
-    PihooksGmsModel="Pixel 6"
+    PihooksGmsFp="google/cheetah_beta/cheetah:16/BP22.250325.007/13352765:user/release-keys" \
+    PihooksGmsModel="Pixel 7 Pro"
 
 # Evolution X packages
 PRODUCT_PACKAGES += \

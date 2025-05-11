bbf8f75cf385f4437e065a3118f8dc25c9d3f65d
Pranav Vashi <neobuddy89@gmail.com>
2025-05-11 20:20:07 +0530

    soong: Add CAMERA_NEEDS_CLIENT_INFO_LIB{_OPLUS} to select()
    Co-authored-by: ShevT <evgeny.shishkov@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -45,6 +45,14 @@ ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
     $(error TARGET_CAMERA_PACKAGE_NAME is deprecated, please migrate to soong_config_set,camera,package_name)
 endif
 
+ifneq ($(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB),)
+    $(error TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB is deprecated, please migrate to soong_config_set,camera,needs_client_info_lib)
+endif
+
+ifneq ($(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS),)
+    $(error TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS is deprecated, please migrate to soong_config_set,camera,needs_client_info_lib_oplus)
+endif
+
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
     $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))

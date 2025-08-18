f742c0f3b102aa339f02db1f3e44ba42b8f20471
Joey Huab <jhuab@edukasyon.ph>
2025-08-18 11:57:09 +0900

    version: Uprev to 10.9
    * September 2025 patches

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION_BASE := 10.7
+EVO_VERSION_BASE := 10.9
 EVO_BUILD_TYPE ?= Unofficial
 
 ifeq ($(WITH_GMS),true)

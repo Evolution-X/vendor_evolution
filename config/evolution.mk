2df0173159de0de8dd2adfa5ca5f73efb06e493b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-27 20:08:36 +0800

    config: add PERF_ANIM_OVERRIDE flag
    Change-Id: I7d264207abef968739b57c9fce19e8f65466ee8c

--- config/evolution.mk
+++ config/evolution.mk
@@ -71,3 +71,15 @@ include vendor/evolution-priv/keys/keys.mk
 else
 -include vendor/evolution-priv/keys/keys.mk
 endif
+
+# PERF_ANIM_OVERRIDE
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.activity_anim_perf_override=$(PERF_ANIM_OVERRIDE)
+
+ifeq ($(PERF_ANIM_OVERRIDE),true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.sf.predict_hwc_composition_strategy=0
+endif
+
+# Other ROM feature flags
+PERF_ANIM_OVERRIDE ?= false

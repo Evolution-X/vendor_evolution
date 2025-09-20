00d6e4d961dbfdf6def1f76bab7b1141ed3df2da
minaripenguin <minaripenguin@users.noreply.github.com>
2025-09-20 09:04:36 +0000

    config: Load boot image profile from frameworks/base
    [YAAP:] art boot image profile is only intended for thin art builds
    without frameworks/base.
    
    Change-Id: I60f3117c6ba948311af9f3fe048d0cf304fc283a

--- config/evolution.mk
+++ config/evolution.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
     ColumbusService
 endif
 
+# Use a generic profile based boot image by default
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
 # Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.arm64.memtag.app.com.android.se=off \

c44cdcef4af39e88d79d622f174fc21aead2e09b
aswin7469 <aswinas@pixysos.com>
2025-12-18 02:15:47 +0530

    overlay: Convert Settings overlay to rro
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -340,7 +340,8 @@ PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
     NetworkStackOverlay \
     NfcOverlay \
-    PermissionControllerOverlay
+    PermissionControllerOverlay \
+    SettingsEvo
 
 # Translations
 CUSTOM_LOCALES += \

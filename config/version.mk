c453d936e569f9fd329af78548ea07a483e4f4d3
Joey <joey@evolution-x.org>
2026-05-05 12:30:22 +0900

    version: Uprev to 9.14
    * May 2026 security patches

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 14
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION := 9.13
+EVO_VERSION := 9.14
 EVO_BUILD_TYPE ?= Unofficial
 
 # Internal version

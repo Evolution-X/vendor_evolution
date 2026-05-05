a1327e1af72aeddd393b25df619bea033e857d36
Joey <joey@evolution-x.org>
2026-05-05 12:42:17 +0900

    version: Uprev to 10.17
    * May 2026 security patches

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION_BASE := 10.16
+EVO_VERSION_BASE := 10.17
 EVO_BUILD_TYPE ?= Unofficial
 
 ifeq ($(WITH_GMS),true)
